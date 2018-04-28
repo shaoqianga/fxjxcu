61994004a7c2bb83f5199c5cdcd74bf1f0b910bf
shaoqiang123 <1767900235@qq.com>
2018-04-28 14:16:53 +0800

    sms code test ->update benstalk config

--- application/common/command/sms_start.php
+++ application/common/command/sms_start.php
@@ -14,28 +14,68 @@ use think\console\Output;
 
 class sms_start extends Command{
 
-
+    /**
+     *启动sms 命令行
+     */
     protected function configure()
     {
         $this->setName('sms_start')
             ->addArgument('name', Argument::OPTIONAL, "sms");
-            //->addOption('city', null, Option::VALUE_REQUIRED, 'city name')
-            //->setDescription('Say Hello');
     }
 
     protected function execute(Input $input, Output $output)
     {
-        echo 'ccccccccc';
-        /*$name = trim($input->getArgument('name'));
-        $name = $name ?: 'thinkphp';
+        $pheanstalk = new \Pheanstalk\Pheanstalk(
+            config('beanstalk.host'),
+            config('beanstalk.port'));
+        while (true){
 
-        if ($input->hasOption('city')) {
-            $city = PHP_EOL . 'From ' . $input->getOption('city');
-        } else {
-            $city = '';
+            if(!$pheanstalk->getConnection()->isServiceListening())
+            {
+                $output->writeln('error connecting to beanstalk ,sleeping for 5
+            second');
+                sleep(5);
+
+                continue;
+            }
+
+            $job = $pheanstalk
+                ->watch(config('beanstalk.SMS'))
+                ->ignore('default')
+                ->reserve();
+
+            $data = $job->getData();
+            $data = json_decode($data,true);
+            try{
+                //send sms code
+                $this->send_sms_code($data['phone'],$data['message']);
+                $pheanstalk->delete($job);
+                $output->writeln('success');
+                sleep(10);
+                //记录发送日志
+            }catch (Exception $e)
+            {
+                //记录失败发送日志
+                $output->writeln('fail');
+            }
         }
+    }
 
-        $output->writeln("Hello," . $name . '!' . $city);*/
+
+    private function send_sms_code($phone,$message){
+
+        header("Content-type: textml; charset=utf-8");
+        date_default_timezone_set('PRC');
+        $uid = 'CDJS001813';
+        $passwd = 'zm0513@';
+
+        $msg = rawurlencode(mb_convert_encoding($message, "gb2312", "utf-8"));
+
+        $gateway = "https://sdk2.028lk.com/sdk2/BatchSend2.aspx?CorpID={$uid}&Pwd={$passwd}&Mobile={$phone}&Content={$msg}&Cell=&SendTime=";
+
+        $result = file_get_contents($gateway);
+
+        return $result;
     }
 
 }
